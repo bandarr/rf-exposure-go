70e2ff431c2d5c9be228b61efea3b8ad6bc4ea4f
Dan Barr <dan.barr@outlook.com>
2022-11-05 22:17:54 -0400

    i did a go

---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var xmtr_power int16 = 1000
+	var feedline_length int16 = 73
+	var duty_cycle float64 = 0.5
+	var per_30 float64 = 0.5
+
+	c1 := CableValues{
+		k1: 0.122290,
+		k2: 0.000260,
+	}
+
+	all_frequency_values := []FrequencyValues{
+		{
+			freq:    7.3,
+			swr:     2.25,
+			gaindbi: 1.5,
+		},
+		{
+			freq:    14.35,
+			swr:     1.35,
+			gaindbi: 1.5,
+		},
+		{
+			freq:    18.1,
+			swr:     3.7,
+			gaindbi: 1.5,
+		},
+		{
+			freq:    21.45,
+			swr:     4.45,
+			gaindbi: 1.5,
+		},
+		{
+			freq:    24.99,
+			swr:     4.1,
+			gaindbi: 1.5,
+		},
+		{
+			freq:    29.7,
+			swr:     2.18,
+			gaindbi: 4.5,
+		},
+	}
+
+	for _, f := range all_frequency_values {
+		var yarg float64 = CalculateUncontrolledSafeDistance(f, c1, xmtr_power, feedline_length, duty_cycle, per_30)
+		fmt.Printf("%6.2f", yarg)
+	}
+}
+
+type CableValues struct {
+	k1 float64
+	k2 float64
+}
+
+type FrequencyValues struct {
+	freq    float64
+	swr     float64
+	gaindbi float64
+}
+
+func CalculateUncontrolledSafeDistance(freq_values FrequencyValues, cable_values CableValues, transmitter_power int16,
+	feedline_length int16, duty_cycle float64, uncontrolled_percentage_30_minutes float64) float64 {
+
+	var gamma float64 = CalculateReflectionCoefficient(freq_values)
+
+	var feedline_loss_per_100ft_at_frequency float64 = CalculateFeedlineLossPer100ftAtFrequency(freq_values, cable_values)
+
+	var feedline_loss_for_matched_load_at_frequency float64 = CalculateFeedlineLossForMatchedLoadAtFrequency(feedline_length, feedline_loss_per_100ft_at_frequency)
+
+	var feedline_loss_for_matched_load_at_frequency_percentage float64 = CalculateFeedlineLossForMatchedLoadAtFrequencyPercentage(feedline_loss_for_matched_load_at_frequency)
+
+	var gamma_squared float64 = math.Pow(math.Abs(gamma), 2)
+
+	var feedline_loss_for_swr float64 = CalculateFeedlineLossForSWR(feedline_loss_for_matched_load_at_frequency_percentage, gamma_squared)
+
+	var feedline_loss_for_swr_percentage float64 = CalculateFeedlineLossForSWRPercentage(feedline_loss_for_swr)
+
+	var power_loss_at_swr float64 = feedline_loss_for_swr_percentage * float64(transmitter_power)
+
+	var peak_envelope_power_at_antenna float64 = float64(transmitter_power) - power_loss_at_swr
+
+	var uncontrolled_average_pep = peak_envelope_power_at_antenna * duty_cycle * uncontrolled_percentage_30_minutes
+
+	var mpe_s = 180 / (math.Pow(freq_values.freq, 2))
+
+	var gain_decimal = math.Pow(10, freq_values.gaindbi/10)
+
+	return math.Sqrt((0.219 * uncontrolled_average_pep * gain_decimal) / mpe_s)
+}
+
+func CalculateReflectionCoefficient(freq_values FrequencyValues) float64 {
+	return math.Abs(float64((freq_values.swr - 1) / (freq_values.swr + 1)))
+}
+
+func CalculateFeedlineLossForMatchedLoadAtFrequency(feedline_length int16, feedline_loss_per_100ft_at_frequency float64) float64 {
+	return ((float64(feedline_length) / 100.0) * feedline_loss_per_100ft_at_frequency)
+}
+
+func CalculateFeedlineLossForMatchedLoadAtFrequencyPercentage(feedline_loss_for_matched_load float64) float64 {
+	return math.Pow(10, (-(feedline_loss_for_matched_load) / 10.0))
+}
+
+func CalculateFeedlineLossPer100ftAtFrequency(freq_values FrequencyValues, cable_values CableValues) float64 {
+	return cable_values.k1 * (math.Sqrt(freq_values.freq + cable_values.k2*freq_values.freq))
+}
+
+func CalculateFeedlineLossForSWR(feedline_loss_for_matched_load_percentage float64, gamma_squared float64) float64 {
+	return -10 * math.Log10(feedline_loss_for_matched_load_percentage*
+		((1-gamma_squared)/(1-math.Pow(feedline_loss_for_matched_load_percentage, 2)*gamma_squared)))
+}
+
+func CalculateFeedlineLossForSWRPercentage(feedline_loss_for_swr float64) float64 {
+	return (100 - 100/(math.Pow(10, feedline_loss_for_swr/10))) / 100
+}
